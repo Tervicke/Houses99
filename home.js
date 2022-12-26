f04523714794f47897517d5e052e587d1b0cd1a6
Tervicke <ameywpatil@gmail.com>
2022-12-26 08:46:10 +0530

    automated making of divs

--- home.js
+++ home.js
@@ -1,4 +1,51 @@
 // loads the navbar
 $(function(){
-  $("#navbar-container").load("navbar.html");
+	$("#navbar-container").load("navbar.html");
 });
+
+$(window).on('load',function(){
+	$.ajax({
+		url :"http://127.0.0.1:5000/data",
+		type:"GET",
+		success: function(response , status) {
+			update_data(response)
+			console.log("works");
+		},
+	})
+
+})
+
+function update_data(response){
+	var tiles_list = [];
+	var banners_list = [];
+	var owners_list = [];
+	var prices_list = [];
+	for(var i = 0 ; i < response.length ; i++){
+		tile = document.createElement('div');
+		banner = document.createElement('img');
+		owner = document.createElement('p');
+		price = document.createElement('p');
+
+		tile.setAttribute('class','tile')
+		banner.setAttribute('class','banner')
+		owner.setAttribute('class','owner')
+		price.setAttribute('class','price')
+
+		owner.innerHTML = "@ " + response[i][1]
+		price.innerHTML = "$ " + response[i][7]
+		banner.src = "data:image/png;base64, " + response[i][6]
+
+		tile.appendChild(banner)
+		tile.appendChild(price)
+		tile.appendChild(owner)
+
+		banners_list.push(banner)
+
+		owners_list.push(owner)
+		tiles_list.push(tile)
+
+		document.getElementById("tiles-container").appendChild(tile);
+	}
+
+	console.log(tiles_list)
+}
