46219ad78ec41a24b82574622ad9e4590b06d6f0
Tervicke <ameywpatil@gmail.com>
2022-12-27 19:40:50 +0530

    removed test functions

--- Api/app.py
+++ Api/app.py
@@ -12,17 +12,7 @@ app = Flask(__name__) #creating the Flask class object
 CORS(app)
 conn = db.connect('data.db' , check_same_thread=False)
 
-@app.route('/') #decorator drfines the   
-def home():  
-    return "test";  
-@app.route('/City/<city_name>')
-def get_according_city(city_name):
-    cur = conn.cursor()
-    print(city_name)
-    print("SELECT * FROM properties where city=%s" % (city_name))
-    cur.execute("SELECT * FROM properties where city=\"%s\";" % (city_name))
-    return jsonify(cur.fetchall().decode("utf-8"))
-
+  
 @app.route('/data')
 def success():
     cur = conn.cursor()
@@ -52,16 +42,5 @@ def get_final_data(raw_data):
         i+=1;
 
     return final_data
-
-@app.route("/image")
-def see_image():
-    cur = conn.cursor()
-    cur.execute("SELECT photo from properties")
-    list1 = cur.fetchall()
-    with open("house.jpg",'rb') as img:
-        base64string = base64.b64encode(img.read()) 
-
-    return jsonify(base64string.decode('utf-8'))
-
 if __name__ =='__main__':  
     app.run(debug = True)
