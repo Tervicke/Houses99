9c2d52cb994fa061420d0dae21816e9332a94d78
Tervicke <ameywpatil@gmail.com>
2022-12-27 15:16:13 +0530

    added viewpage

--- home.js
+++ home.js
@@ -22,9 +22,62 @@ $(window).on('load',function(){
 
 })
 
-function clicked(el){
+function clicked(el , response){
+	console.log(tile)
 	tile = el.target.parentNode
 	console.log(tile.id)
+	document.getElementById('tiles-container').remove()
+	view = document.createElement('div');
+	view.setAttribute('id','view')
+	document.body.appendChild(view)
+
+	container =  document.createElement('div')
+	container.setAttribute('id','container')
+	document.getElementById('view').appendChild(container)
+
+	image = document.createElement('img')
+	image.setAttribute('id','view-image')
+
+	document.getElementById('container').appendChild(image)
+	image.src = "data:image/png;base64, " + response[tile.id][6]
+	owners_info = document.createElement('div')
+	owners_info.setAttribute('id','owners-info')
+	owners_info.style.height = image.height.toString();
+	container.appendChild(owners_info)
+
+	owner_name = document.createElement('p')
+	owner_name.innerHTML = "Name : " + response[tile.id][1]
+	owner_name.setAttribute('id','owner-name')
+	owners_info.appendChild(owner_name)
+
+	owner_no= document.createElement('p')
+	owner_no.innerHTML = "Contact : " + response[tile.id][2]
+	owner_no.setAttribute('id','owner-no')
+	owners_info.appendChild(owner_no)
+
+	owner_adress= document.createElement('p')
+	owner_adress.innerHTML = "Adress: " + response[tile.id][5]
+	owner_adress.setAttribute('id','owner-adress')
+	owners_info.appendChild(owner_adress)
+
+	rent = document.createElement('p')
+	rent.innerHTML = "Rent : $ " + response[tile.id][7]
+	rent.setAttribute('id','rent')
+	owners_info.appendChild(rent)
+
+	email = document.createElement('button')
+	email.setAttribute('id','email-btn')
+	email.innerHTML = 'Email'
+	owners_info.appendChild(email)
+
+
+	rentnow = document.createElement('button')
+	rentnow.setAttribute('id','rentnow-btn')
+	rentnow.innerHTML = 'Rent now'
+	owners_info.appendChild(rentnow)
+
+	console.log(email)
+	console.log('helloworld')
 }
 
 
@@ -41,7 +94,7 @@ function update_data(response){
 		price = document.createElement('p');
 
 		tile.addEventListener("click", function(el){
-			clicked(el);
+			clicked(el,response);
 		});
 
 		tile.setAttribute('class','tile')
